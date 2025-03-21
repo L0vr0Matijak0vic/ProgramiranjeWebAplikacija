e5c1263ef9ac6a199d124486af48848e2482b296
MatijakovicLovro <lovro.matijakovic@tvz.hr>
2025-03-21 13:07:45 +0100

    Vjezba_1.php
    Prva vjezba kolegija

---
+++ Vjezba_1/Vjezba1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html lang="hr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="DaVincijev kod">
+        <meta name="keywords" content="DaVincijev kod, Dan Brown, knjiga">
+        <meta name="author" content="Lovro Matijaković">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>DaVincijev kod</title>
+
+    <body>
+        <?php
+            echo "<h1>Davijncijev kod</h1>";
+            echo "<p>DaVincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>";
+            echo "<a href = 'https://hr.wikipedia.org/wiki/DaVincijev_kod' target = '_blank' >Više o knjizi</a>";
+            // Komentar je ime domuneta tj. Vjezba1.php
+        ?>
+    </body>
+</html>
